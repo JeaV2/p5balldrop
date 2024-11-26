5ad6cfdadf3fe3fe6d182659b5205b925d878df0
JeaV2 <102710@glr.nl>
2024-11-26 11:12:37 +0100

    Fix velocity input

--- sketch.js
+++ sketch.js
@@ -13,17 +13,19 @@ function setup() {
   createCanvas(canvasWidth, canvasHeight);
 
   // Add event listener for velocity input
-  document.getElementById('velocityInput').addEventListener('input', function(event) {
-    velocity = parseFloat(event.target.value) || 0;
+  document.getElementById('velocitySubmit').addEventListener('click', function() {
+    velocity = parseFloat(document.getElementById('velocityInput').value) || 0;
   });
+  // Add event listener for gravity input
   document.getElementById('gravityInput').addEventListener('input', function(event) {
-    gravity = parseFloat(event.target.value) /10 || 0;
+    gravity = parseFloat(event.target.value) / 10 || 0;
   });
 }
 
 // Drop the ball again
 function dropBall() {
   ballYPosition = 0;
+  velocity = 0;
 }
 
 // Handle key presses
@@ -45,12 +47,13 @@ function draw() {
   // Check for collision with the floor
   if (ballYPosition > floorHeight - 50) {
     ballYPosition = floorHeight - 50;
-    // Reverse velocity and apply damping
+    // Play bounce sound if velocity is high enough
     if (Math.abs(velocity) > bounceThreshold) {
       let bounceSound = new Audio('content/audio/ball.mp3');
       bounceSound.play();
       console.log('bounce');
     }
+    // Reverse velocity and apply damping
     velocity *= -0.9;
   }
 
@@ -62,14 +65,14 @@ function draw() {
   // Draw the floor
   fill('#444');
   rect(0, floorHeight, canvasWidth, 100);
+  
   // stats
   velocityStat = document.getElementById('velocity').innerHTML = Math.round(velocity * 1000)/1000;
   gravityStat = document.getElementById('gravity').innerHTML = gravity;
-  ballYPositionStat = document.getElementById('ballYPosition').innerHTML = Math.round((canvasHeight - ballYPosition - 75) * 100)/100;
+  ballHeightStat = document.getElementById('ballHeight').innerHTML = Math.round((canvasHeight - ballYPosition - 75) * 100)/100;
   canvasHeightStat = document.getElementById('canvasHeight').innerHTML = canvasHeight;
   canvasWidthStat = document.getElementById('canvasWidth').innerHTML = canvasWidth;
   floorHeightStat = document.getElementById('floorHeight').innerHTML = canvasHeight - floorHeight;
-
 }
 
 function windowResized() {
